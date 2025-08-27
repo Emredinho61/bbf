fa9875317651f97c5c0e73eaf13c6c9b3e415da0
Emre Bayazitoglu <emre-samet61@hotmail.de>
2025-08-27 19:47:17 +0200

    corrected Ahmeds faulty response attempt

--- dev/lib/backend/services/uno_to_flask_service.dart
+++ dev/lib/backend/services/uno_to_flask_service.dart
@@ -2,6 +2,12 @@ import 'package:http/http.dart' as http;
 
 class UnoToFlaskService {
   Future<http.Response> fetchAlbum() {
+    var response = http.get(Uri.parse('http://192.168.1.10:5000'));
+    print("Response");
+    print(response.then((data) => {
+      print(data.body),
+      print(data.statusCode)
+    }));
     return http.get(Uri.parse('http://192.168.1.10:5000'));
   }
 }
