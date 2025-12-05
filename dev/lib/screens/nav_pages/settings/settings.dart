a4f65431f74684787b210cc19cb9dbf243e88238
Emredinho61 <emre-samet61@hotmail.de>
2025-12-05 18:18:13 +0100

    outsourced donations in own component for reusability

--- dev/lib/screens/nav_pages/settings/settings.dart
+++ dev/lib/screens/nav_pages/settings/settings.dart
@@ -11,13 +11,12 @@ import 'package:bbf_app/utils/constants/colors.dart';
 import 'package:bbf_app/utils/helper/check_user_helper.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:provider/provider.dart';
 import 'package:bbf_app/backend/services/auth_services.dart';
 import 'package:bbf_app/backend/services/settings_service.dart';
 import 'package:bbf_app/utils/theme/theme_provider.dart';
 import 'package:bbf_app/components/auth_dialog.dart';
-import 'package:url_launcher/url_launcher.dart';
+import "package:bbf_app/components/donations/donation_tile.dart";
 
 class SettingsPage extends StatefulWidget {
   SettingsPage({super.key});
@@ -721,126 +720,4 @@ class LightDarkModeSwitch extends StatelessWidget {
   }
 }
 
-class BankInfoCard extends StatelessWidget {
-  const BankInfoCard({super.key, required this.isDark});
 
-  final bool isDark;
-
-  @override
-  Widget build(BuildContext context) {
-    // Copyable text
-    Widget copyableRow(String label, String value) {
-      return Row(
-        children: [
-          Expanded(
-            child: Text(
-              "$label: $value",
-              style: Theme.of(context).textTheme.labelLarge,
-            ),
-          ),
-          IconButton(
-            icon: const Icon(Icons.copy, size: 18),
-            onPressed: () {
-              Clipboard.setData(ClipboardData(text: value));
-              ScaffoldMessenger.of(
-                context,
-              ).showSnackBar(SnackBar(content: Text("$label copied!")));
-            },
-          ),
-        ],
-      );
-    }
-
-    return Container(
-      margin: EdgeInsets.symmetric(horizontal: 10),
-      padding: EdgeInsets.all(10),
-      decoration: BoxDecoration(
-        border: Border.all(color: isDark ? Colors.white : Colors.green),
-      ),
-      child: ListTile(
-        leading: const Icon(Icons.account_balance),
-        title: Center(
-          child: Text(
-            "Bildungs- und Begegnungsverein Freiburg e.V.",
-            style: Theme.of(context).textTheme.bodyMedium,
-          ),
-        ),
-        subtitle: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            copyableRow("IBAN", "DE11 6805 0101 0014 3501 24"),
-            copyableRow("BIC", "FRSPDE66XXX"),
-            copyableRow("Verwendungszweck", "Spende"),
-          ],
-        ),
-      ),
-    );
-  }
-}
-
-class DividerWithText extends StatelessWidget {
-  const DividerWithText({super.key, required this.isDark});
-
-  final bool isDark;
-
-  @override
-  Widget build(BuildContext context) {
-    return Center(
-      child: Row(
-        mainAxisSize: MainAxisSize.min,
-        children: [
-          SizedBox(
-            width: 50,
-            child: Divider(
-              color: isDark ? Colors.white : Colors.black,
-              thickness: 1,
-            ),
-          ),
-          SizedBox(width: 10),
-          Text('oder'),
-          SizedBox(width: 10),
-          SizedBox(
-            width: 50,
-            child: Divider(
-              color: isDark ? Colors.white : Colors.black,
-              thickness: 1,
-            ),
-          ),
-        ],
-      ),
-    );
-  }
-}
-
-class IconButtonForPayPal extends StatelessWidget {
-  const IconButtonForPayPal({super.key, required this.isDark});
-
-  final bool isDark;
-
-  @override
-  Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: () async {
-        final Uri url = Uri.parse(
-          'https://www.paypal.com/donate/?hosted_button_id=ESTNXJLMMQQQS#',
-        );
-        if (!await launchUrl(url, mode: LaunchMode.externalApplication)) {
-          debugPrint('Konnte $url nicht öffnen');
-        }
-      },
-      child: Align(
-        alignment: Alignment.center,
-        child: Container(
-          height: 60,
-          width: 60,
-          decoration: BoxDecoration(
-            color: isDark ? Colors.green.shade300 : Colors.green.shade200,
-            border: Border.all(color: Colors.white),
-            borderRadius: BorderRadius.circular(12),
-          ),
-          child: Image.asset('assets/images/PayPalLogo.png'),
-        ),
-      ),
-    );
-  }
-}
